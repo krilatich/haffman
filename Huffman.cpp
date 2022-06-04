cec2a788923ed36d604c4cf9dd9ec07bbf81c528
krilatich <106861958+krilatich@users.noreply.github.com>
2022-06-04 19:47:45 +0700

    Add files via upload

--- Huffman.cpp
+++ Huffman.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-
+#include <vector>
+#include <map>
+#include <list>
+#include <fstream>
 
 
 using namespace std;
@@ -13,7 +16,72 @@ public:
 
 	Node() { left = right = NULL; }
 
-	
+	Node(Node* L, Node* R)
+	{
+		left = L;
+		right = R;
+		a = L->a + R->a;
+	}
+
 };
 
 
+struct MyCompare
+{
+	bool operator()(const Node* l, const Node* r) const { return l->a < r->a; }
+};
+
+
+
+vector<bool> code;
+map<char, vector<bool> > table;
+
+
+
+int main(int argc, char* argv[])
+{
+
+	ifstream f("1.txt", ios::out | ios::binary);
+
+	map<char, int> m;
+
+	while (!f.eof())
+	{
+		char c = f.get();
+		m[c]++;
+	}
+
+
+
+	list<Node*> t;
+	for (map<char, int>::iterator itr = m.begin(); itr != m.end(); ++itr)
+	{
+		Node* p = new Node;
+		p->c = itr->first;
+		p->a = itr->second;
+		t.push_back(p);
+	}
+
+
+
+
+	while (t.size() != 1)
+	{
+		t.sort(MyCompare());
+
+		Node* SonL = t.front();
+		t.pop_front();
+		Node* SonR = t.front();
+		t.pop_front();
+
+		Node* parent = new Node(SonL, SonR);
+		t.push_back(parent);
+
+	}
+
+	Node* root = t.front();
+
+
+
+	return 0;
+}
